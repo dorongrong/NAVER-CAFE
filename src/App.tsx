96cd8cc7b5417d281843847585ab479b42f290e4
dorongrong <84131419+dorongrong@users.noreply.github.com>
2022-11-14 10:47:23 +0900

    Git Development Setting

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React and Git setting
+          Git Development setting
         </a>
       </header>
     </div>
